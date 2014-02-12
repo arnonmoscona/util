f02e67eef0ccf3633ed5f9c703969fd18871ba27
Arnon Moscona <arnon@moscona.com>
2014-02-12 08:00:46 -0800

    NJ-250 Use IQFeed for historical data - added StringHelper from older project

---
+++ src/main/java/com/moscona/util/StringHelper.java
@@ -0,0 +1,159 @@
+package com.moscona.util;
+
+// FIXME Unit tests
+// FIXME Refactor to make this Java8 friendly
+// import com.moscona.dataSpace.IScalar;  // FIXME this needs to be an extension in dataSpace, not here
+import com.moscona.exceptions.InvalidArgumentException;
+import org.apache.commons.beanutils.BeanUtils;
+import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.lang3.builder.ReflectionToStringBuilder;
+import org.apache.commons.lang3.builder.ToStringStyle;
+
+
+import java.util.*;
+
+/**
+ * Created: Jun 15, 2010 4:25:26 PM
+ * By: Arnon Moscona
+ */
+@SuppressWarnings({"UtilityClass"})
+public class StringHelper {
+    private StringHelper() {
+
+    }
+
+    public static String join(String[] list, String delimiter) {
+        return StringUtils.join(list, delimiter);
+    }
+
+    public static String join(Collection c, String delimiter) {
+        return StringUtils.join(c,delimiter);
+    }
+
+    public static String toPaddedString(long i,int length) {
+        return StringUtils.leftPad(Long.toString(i),length,'0');
+    }
+
+    public static String toPaddedString(long i,int length, char padChar) {
+        return StringUtils.leftPad(Long.toString(i),length,padChar);
+    }
+
+    public static String toPaddedString(long i) {
+        return toPaddedString(i,2);
+    }
+
+    public static boolean parseBoolean(String b) throws InvalidArgumentException {
+        String strValue = b.trim().toLowerCase();
+        if (strValue.equals("true") || strValue.equals("yes") || strValue.equals("1") || strValue.equals("on")) {
+            return true;
+        }
+        if (strValue.equals("false") || strValue.equals("no") || strValue.equals("0") || strValue.equals("off")) {
+            return false;
+        }
+        throw new InvalidArgumentException("The value \""+b+"\" could not be parsed as a boolean");
+    }
+
+    /**
+     * A generic toString() method for any object. Uses BeanUtils. No guarantees.
+     * @param bean
+     * @return
+     */
+    public static String toString(Object bean) {
+        try {
+            return ReflectionToStringBuilder.toString(bean, ToStringStyle.SHORT_PREFIX_STYLE);
+        }
+        catch (Exception e) {
+            return "Error generating toString() for "+bean.getClass().getName()+": "+e;
+        }
+    }
+
+    public static String prettyPrint(double num) {
+        if (Math.abs(num) < 0.01) {
+            return new Formatter().format("%,.5f", num).toString();
+        }
+        if (Math.abs(num) < 1.0) {
+            return new Formatter().format("%,.3f", num).toString();
+        }
+        return new Formatter().format("%,.2f", num).toString();
+    }
+
+    public static String prettyPrint(long num) {
+        return new Formatter().format("%,d", num).toString();
+    }
+
+    public static String prettyPrint(Number value) {
+        if (value instanceof Double || value instanceof Float) {
+            return prettyPrint(value.doubleValue());
+        }
+        return prettyPrint(value.longValue());
+    }
+
+    public static String wrap(String str, int maxWidth) {
+        if (str.length() < maxWidth) {
+            return str;
+        }
+
+        StringBuilder s = new StringBuilder();
+        int currentLineLength = 0;
+        for (String word: str.trim().split("[ \t]")) {
+            if (currentLineLength>0 && currentLineLength+word.length() > maxWidth) {
+                s.append("\n");
+                currentLineLength = 0;
+            }
+            if (currentLineLength > 0) {
+                s.append(" ");
+                currentLineLength++;
+            }
+            s.append(word);
+            currentLineLength += word.length();
+        }
+
+        return s.toString();
+    }
+
+    // FIXME this needs to be an extension in dataSpace, not here
+//    public static String toString(Map<String, IScalar> row, String indent) {
+//        StringBuilder sb = new StringBuilder();
+//        sb.append("[\n");
+//        for (String key: row.keySet()) {
+//            sb.append(indent).append("\"").append(key).append("\": ").append(row.get(key)).append("\n");
+//        }
+//        sb.append("]\n");
+//        return sb.toString();
+//    }
+
+    public static String[] asArray(List<String> list) {
+        String[] arr = new String[list.size()];
+        for (int i=0; i<list.size(); i++) {
+            arr[i] = list.get(i);
+        }
+        return arr;
+    }
+
+    public static String camelCase(String englishName) {
+        String[] parts = englishName.split(" +");
+        StringBuilder sb = new StringBuilder();
+        boolean isFirst = true;
+        for (String s: parts) {
+            sb.append(isFirst ? s : StringUtils.capitalize(s));
+            isFirst = false;
+        }
+        return sb.toString();
+    }
+
+    public static String camelToEnglish(String s) {
+        String[] words = StringUtils.splitByCharacterTypeCamelCase(s);
+        return StringUtils.join(words," ").toLowerCase();
+    }
+
+//    public static String englishToCamel(String findingEnglish) {
+//        String[] words = findingEnglish.split(" +");
+//        String result = "";
+//        boolean isFirst = true;
+//        for (String word: words) {
+//            result += (isFirst ? word : StringUtils.capitalize(word));
+//            isFirst = false;
+//        }
+//        return result;
+//    }
+}
---
+++ src/test/java/com/moscona/util/StringHelperTest.java
@@ -0,0 +1,196 @@
+package com.moscona.util;
+
+import org.junit.Test;
+import org.junit.Before;
+import org.junit.After;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.Assert.*;
+
+
+/**
+ * StringHelper Tester.
+ *
+ * @author Arnon Moscona
+ * @version 1.0
+ * @since <pre>Feb 10, 2014</pre>
+ */
+public class StringHelperTest {
+
+    // FIXME these tests are very partial. Need to add many more...
+
+    private static class Bean {
+        private int i = -1;
+        private double d = Math.PI;
+        private String s = "name";
+
+        public Bean() {}
+
+        public int getI() {
+            return i;
+        }
+
+        public void setI(int i) {
+            this.i = i;
+        }
+
+        public double getD() {
+            return d;
+        }
+
+        public void setD(double d) {
+            this.d = d;
+        }
+
+        public String getS() {
+            return s;
+        }
+
+        public void setS(String s) {
+            this.s = s;
+        }
+    }
+
+    @Before
+    public void before() throws Exception {
+    }
+
+    @After
+    public void after() throws Exception {
+    }
+
+    /**
+     * Method: join(String[] list, String delimiter)
+     */
+    @Test
+    public void testJoinForListDelimiter() throws Exception {
+        String result = StringHelper.join(new String[]{"1", "2", "3"}, ", ");
+        assertEquals("Should match commons", "1, 2, 3", result);
+    }
+
+    /**
+     * Method: join(Collection c, String delimiter)
+     */
+    @Test
+    public void testJoinForCDelimiter() throws Exception {
+        String result = StringHelper.join(Arrays.asList("1", "2", "3"), ", ");
+        assertEquals("Should match commons", "1, 2, 3", result);
+    }
+
+    /**
+     * Method: toPaddedString(long i, int length)
+     */
+    @Test
+    public void testToPaddedStringForILength() throws Exception {
+        String result = StringHelper.toPaddedString(123, 5);
+        assertEquals("00123", result);
+    }
+
+    /**
+     * Method: toPaddedString(long i, int length, char padChar)
+     */
+    @Test
+    public void testToPaddedStringForILengthPadChar() throws Exception {
+        String result = StringHelper.toPaddedString(123, 5, ' ');
+        assertEquals("  123", result);
+    }
+
+    /**
+     * Method: toPaddedString(long i)
+     */
+    @Test
+    public void testToPaddedStringI() throws Exception {
+        String result = StringHelper.toPaddedString(1);
+        assertEquals("01", result);
+    }
+
+    /**
+     * Method: parseBoolean(String b)
+     */
+    @Test
+    public void testParseBoolean() throws Exception {
+        for (String t: Arrays.asList("true", "True", "TRUE", "1", "on", "On", "ON", "yes", "Yes", "YES")) {
+            assertTrue("\""+t+"\" should be true", StringHelper.parseBoolean(t));
+        }
+        for (String f: Arrays.asList("false", "False", "FALSE", "0", "off", "Off", "OFF", "no", "No", "NO")) {
+            assertFalse("\""+f+"\" should be false", StringHelper.parseBoolean(f));
+        }
+    }
+
+    /**
+     * Method: toString(Object bean)
+     */
+    @Test
+    public void testToString() throws Exception {
+        String result = StringHelper.toString(new Bean());
+        assertEquals("Bean representation incorrect", "StringHelperTest.Bean[i=-1,d=3.141592653589793,s=name]", result);
+    }
+
+    /**
+     * Method: prettyPrint(double num)
+     */
+    @Test
+    public void testPrettyPrintNum() throws Exception {
+        assertEquals("Double pretty print incorrect", "3.14", StringHelper.prettyPrint(Math.PI));
+        assertEquals("Large double pretty print incorrect", "3,141.59", StringHelper.prettyPrint(Math.PI*1000));
+        assertEquals("Large integer pretty print incorrect", "1,024", StringHelper.prettyPrint(1024));
+    }
+
+    /**
+     * Method: prettyPrint(Number value)
+     */
+    @Test
+    public void testPrettyPrintValue() throws Exception {
+        assertEquals("Number pretty print incorrect", "3.14", StringHelper.prettyPrint(new Double(Math.PI)));
+    }
+
+    /**
+     * Method: wrap(String str, int maxWidth)
+     */
+    @Test
+    public void testWrap() throws Exception {
+        assertEquals("incorrect string wrap1", "123\n456\n7", StringHelper.wrap("123 456 7",3));
+        assertEquals("incorrect string wrap2", "1 2\n3\n456\n7", StringHelper.wrap("1 2\t3 456 7",3));
+        assertEquals("incorrect string wrap3", "123\n456\n789xxx", StringHelper.wrap("123 456 789xxx",3));
+    }
+
+    /**
+     * Method: asArray(List<String> list)
+     */
+    @Test
+    public void testAsArray() throws Exception {
+        String[] original = new String[] {"1","2","3"};
+        List<String> list = Arrays.asList(original);
+        String[] result = StringHelper.asArray(list);
+        assertArrayEquals("toArray failed", original, result);
+    }
+
+    /**
+     * Method: camelCase(String englishName)
+     */
+    @Test
+    public void testCamelCase() throws Exception {
+        assertEquals("camelCase", StringHelper.camelCase("camel case"));
+    }
+
+    /**
+     * Method: camelToEnglish(String s)
+     */
+    @Test
+    public void testCamelToEnglish() throws Exception {
+        assertEquals("camel case", StringHelper.camelToEnglish("camelCase"));
+        assertEquals("camel case", StringHelper.camelToEnglish("CamelCase"));
+    }
+
+    /**
+     * Method: englishToCamel(String findingEnglish)
+     */
+//    @Test
+//    public void testEnglishToCamel() throws Exception {
+//        assertEquals("camelCase", StringHelper.englishToCamel("camel case"));
+//    }
+
+
+} 
